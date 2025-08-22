2fc1c7c8d3bc610d96ef787c1d159076644c7fd0
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-08-22 09:16:28 +0530

    Add files via upload

---
+++ ChatServer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<TcpClient> clients = new List<TcpClient>();
+
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            clients.Add(client);
+            Console.WriteLine("Client connected!");
+            Thread thread = new Thread(HandleClient);
+            thread.Start(client);
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+
+        try
+        {
+            while (true)
+            {
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                if (bytes == 0) break;
+
+                string message = Encoding.UTF8.GetString(buffer, 0, bytes);
+                Console.WriteLine("Received: " + message);
+
+                // Broadcast to all clients
+                foreach (var c in clients)
+                {
+                    if (c != client)
+                    {
+                        NetworkStream s = c.GetStream();
+                        s.Write(buffer, 0, bytes);
+                    }
+                }
+            }
+        }
+        finally
+        {
+            clients.Remove(client);
+            client.Close();
+        }
+    }
+}
