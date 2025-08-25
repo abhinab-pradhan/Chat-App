9622d27e32b56e8f6d07ddbd12685ebf68550527
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-08-25 13:40:06 +0530

    Update Form1.cs

--- ChatClient/Form1.cs
+++ ChatClient/Form1.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace ChatClient
 {
@@ -13,7 +14,7 @@ namespace ChatClient
 
         TextBox inputBox;
         Button sendButton;
-        RichTextBox chatBox;
+        FlowLayoutPanel chatPanel;
 
         public Form1()
         {
@@ -26,22 +27,27 @@ namespace ChatClient
         {
             this.Text = "Chat Client";
             this.Width = 400;
-            this.Height = 500;
+            this.Height = 600;
 
-            chatBox = new RichTextBox();
-            chatBox.Location = new System.Drawing.Point(10, 10);
-            chatBox.Size = new System.Drawing.Size(360, 350);
-            chatBox.ReadOnly = true;
-            this.Controls.Add(chatBox);
+            // Chat panel
+            chatPanel = new FlowLayoutPanel();
+            chatPanel.Location = new Point(10, 10);
+            chatPanel.Size = new Size(360, 480);
+            chatPanel.AutoScroll = true;
+            chatPanel.FlowDirection = FlowDirection.TopDown;
+            chatPanel.WrapContents = false;
+            this.Controls.Add(chatPanel);
 
+            // Input box
             inputBox = new TextBox();
-            inputBox.Location = new System.Drawing.Point(10, 370);
-            inputBox.Size = new System.Drawing.Size(260, 30);
+            inputBox.Location = new Point(10, 500);
+            inputBox.Size = new Size(260, 30);
             this.Controls.Add(inputBox);
 
+            // Send button
             sendButton = new Button();
             sendButton.Text = "Send";
-            sendButton.Location = new System.Drawing.Point(280, 370);
+            sendButton.Location = new Point(280, 500);
             sendButton.Click += SendButton_Click;
             this.Controls.Add(sendButton);
         }
@@ -57,7 +63,7 @@ namespace ChatClient
                 thread.IsBackground = true;
                 thread.Start();
 
-                chatBox.AppendText("Connected to server...\n");
+                AddMessage("Connected to server...", false);
             }
             catch (Exception ex)
             {
@@ -73,7 +79,7 @@ namespace ChatClient
             byte[] data = Encoding.UTF8.GetBytes(message);
             stream.Write(data, 0, data.Length);
 
-            chatBox.AppendText("Me: " + message + "\n");
+            AddMessage(message, true);
             inputBox.Clear();
         }
 
@@ -89,11 +95,38 @@ namespace ChatClient
 
                     this.Invoke((MethodInvoker)delegate
                     {
-                        chatBox.AppendText("Friend: " + message + "\n");
+                        AddMessage(message, false);
                     });
                 }
                 catch { break; }
             }
+        }
+
+        private void AddMessage(string text, bool isMine)
+        {
+            Label msgLabel = new Label();
+            msgLabel.Text = text;
+            msgLabel.AutoSize = true;
+            msgLabel.MaximumSize = new Size(200, 0);
+            msgLabel.Padding = new Padding(8);
+            msgLabel.Margin = new Padding(3);
+
+            if (isMine)
+            {
+                msgLabel.BackColor = Color.LightGreen;
+                msgLabel.TextAlign = ContentAlignment.MiddleRight;
+                msgLabel.Anchor = AnchorStyles.Right;
+                msgLabel.Left = chatPanel.Width - msgLabel.Width - 30;
+            }
+            else
+            {
+                msgLabel.BackColor = Color.LightGray;
+                msgLabel.TextAlign = ContentAlignment.MiddleLeft;
+                msgLabel.Anchor = AnchorStyles.Left;
+            }
+
+            chatPanel.Controls.Add(msgLabel);
+            chatPanel.ScrollControlIntoView(msgLabel);
         }
     }
 }
